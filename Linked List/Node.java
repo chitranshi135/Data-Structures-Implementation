0a5606ee59d40691a315b9e72d5f60e4c85fa92a
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2022-10-02 12:04:01 +0530

    Add files via upload

---
+++ Linked List/Node.java
@@ -0,0 +1,15 @@
+//class Node<T>
+//{
+//    T data;
+//    Node<T> next;
+//    Node(T value)
+//    {
+//        this.data = value;
+//        this.next = null;
+//    }
+//    Node(T element , Node<T> address)
+//    {
+//        this.data = element;
+//        this.next = address;
+//    }
+//}
